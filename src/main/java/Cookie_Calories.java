ba4e3f56e392d94cbd4984cf87c75b3bc5c52dfa
kylep <kprater@gus.pittstate.edu>
2020-09-01 22:29:32 -0500

    Initial Commit

---
+++ src/main/java/Cookie_Calories.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mdpra
+ */
+public class Cookie_Calories {
+    public static void main (String[] args){
+        //Calculate the Number of Calories per Bag of Cookies
+        final double CALORIES_PER_SERVING = 300; 
+        final double COOKIES_IN_BAG = 40;
+        double CaloriesPerBag = CALORIES_PER_SERVING * 10; 
+        
+        //Calculate the Number of Cookies per Serving 
+        double CookiesPerServing = COOKIES_IN_BAG / 10; 
+        
+        //Calculate the Number of Calories per Cookie 
+        double CaloriesPerCookie = CALORIES_PER_SERVING / CookiesPerServing;
+        
+        // Ask the user to enter the number of cookies they ate 
+        Scanner keyboard = new Scanner(System.in); 
+        System.out.println("Enter the number of cookies that you ate: ");
+        
+        // Calculate the number of calories the user ate
+        double CookiesAte = keyboard.nextDouble();
+        double CaloriesConsumed = CaloriesPerCookie * CookiesAte; 
+        System.out.println("You consumed " + CaloriesConsumed + " Calories");
+    }
+}
---
+++ src/main/java/Sales_Tax.java
@@ -0,0 +1,45 @@
+
+import java.util.Scanner;
+import java.text.DecimalFormat;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mdpra
+ */
+public class Sales_Tax {
+    private static DecimalFormat df = new DecimalFormat("0.00");
+    public static void main (String[] args){
+        //Get the Purchase Amount and Display it
+        
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter the amount of your purchase: ");
+        double PurchaseAmount = keyboard.nextDouble(); 
+        System.out.println("Your purchase amount is: " + df.format(PurchaseAmount));
+        
+        //Calculate County Sales Tax and Dispaly it
+        final double COUNTY_SALES_TAX_AMMOUNT = 0.02;
+        double CountySalesTax = PurchaseAmount * COUNTY_SALES_TAX_AMMOUNT;
+        System.out.println("County Sales Tax: " + df.format(CountySalesTax));
+        
+        //Calculate State Sales Tax and Display it 
+        final double STATE_SALES_TAX_AMMOUNT = 0.04; 
+        double StateSalesTax = PurchaseAmount * STATE_SALES_TAX_AMMOUNT;
+        System.out.println("State Sales Tax: " + df.format(StateSalesTax));
+        
+        //Calculate Total Sales Tax and Display it 
+        double TotalSalesTax = CountySalesTax + StateSalesTax; 
+        System.out.println("Total Sales Tax: " + df.format(TotalSalesTax));
+        
+        
+        // Calculate the Total of the Sale and Displaly it 
+        double SaleTotal = PurchaseAmount + TotalSalesTax; 
+        System.out.println("Your sales total is: " + df.format(SaleTotal));
+                
+    }
+    
+}
